982a1e3874b71358d2b81621a694b745772b0789
Haru2421 <dk87210530@icloud.com>
2026-09-12 13:19:26 +0900

    確率に重みを付けてマスタの確率を変更させました。

--- src/InternApi/Services/GachaService.cs
+++ src/InternApi/Services/GachaService.cs
@@ -72,7 +72,19 @@ public class GachaService
 
         // TODO(メイン課題1): 下の1行を、重み付き抽選ロジックに書き換えよう。
         // 現状は「プール先頭固定」= 何度引いても同じアイテムしか出ない状態。
-        return gachaDetailMasters[0].ItemId;
+        var totalWeight = gachaDetailMasters.Sum(x => x.Weight);
+var random = Random.Shared.Next(1, totalWeight + 1);
+
+foreach (var detail in gachaDetailMasters)
+{
+    random -= detail.Weight;
+
+    if (random <= 0)
+        return detail.ItemId;
+}
+
+throw new InvalidOperationException("抽選に失敗しました。");
+
     }
 
     /// <summary>
